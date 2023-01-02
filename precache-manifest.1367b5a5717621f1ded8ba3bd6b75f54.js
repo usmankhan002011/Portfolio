5d282d34a126ba8685557c0368ea2b82dab13d2b
usmankhan002011 <117730362+usmankhan002011@users.noreply.github.com>
2023-01-02 20:55:56 +0900

    Updates

---
+++ precache-manifest.1367b5a5717621f1ded8ba3bd6b75f54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761f03a2fb4d5931c3824adee278f241",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "4291886b5bcfae3323e4",
+    "url": "/Portfolio/static/css/2.7921d26e.chunk.css"
+  },
+  {
+    "revision": "f880f4afd7b81a518cbf",
+    "url": "/Portfolio/static/css/main.4069a18c.chunk.css"
+  },
+  {
+    "revision": "4291886b5bcfae3323e4",
+    "url": "/Portfolio/static/js/2.bb12f6ba.chunk.js"
+  },
+  {
+    "revision": "ab4784922d7f45cc6cfd5a83588623bb",
+    "url": "/Portfolio/static/js/2.bb12f6ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f880f4afd7b81a518cbf",
+    "url": "/Portfolio/static/js/main.bb363572.chunk.js"
+  },
+  {
+    "revision": "6da991f8ce13346ac7c3",
+    "url": "/Portfolio/static/js/runtime-main.0f6748c4.js"
+  }
+]);
